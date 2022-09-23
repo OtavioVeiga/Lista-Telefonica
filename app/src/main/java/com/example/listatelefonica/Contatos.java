06dbf5ed687aef6a32341346b016fc62187534a2
Otavio <802.263@alunos.unigran.br>
2022-09-23 10:22:12 -0400

    Class Contatos Criado

--- app/src/main/java/com/example/listatelefonica/Contatos.java
+++ app/src/main/java/com/example/listatelefonica/Contatos.java
@@ -1,5 +1,41 @@
 package com.example.listatelefonica;
 
 public class Contatos {
+    String telefone;
+    Integer id;
+    String datanasc;
 
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDatanasc() {
+        return datanasc;
+    }
+
+
+    public void setDatanasc(String datanasc) {
+        this.datanasc = datanasc;
+
+    }
+    @Override
+    public String toString() {
+        return "Contatos{" +
+                "telefone='" + telefone + '\'' +
+                ", id=" + id +
+                ", datanasc='" + datanasc + '\'' +
+                '}';
+    }
 }
